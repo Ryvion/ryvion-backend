b684b455ecaca015a08bb80e9f5562c7285a03c7
Altrocore <shepelev.daniel.ca@gmail.com>
2025-10-28 19:38:57 -0230

    initial commit

---
+++ src/main/java/com/ryvion/backend/RyvionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ryvion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyvionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyvionBackendApplication.class, args);
+	}
+
+}
