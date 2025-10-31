057b2be3e264eb901c100e9ae26f846ded415b9f
DanfirTach <shepelev_daniel@proton.me>
2025-10-31 11:35:49 -0230

    StrategyRepository - findByUserId, findByStatus

--- src/main/java/com/ryvion/backend/repository/StrategyRepository.java
+++ src/main/java/com/ryvion/backend/repository/StrategyRepository.java
@@ -7,4 +7,7 @@ import java.util.List;
 
 public interface StrategyRepository extends JpaRepository<Strategy,Long> {
 
+    List<Strategy> findByUserId(Long userId);
+
+    List<Strategy> findByStatus(String status);
 }
